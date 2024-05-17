1fd30986672159d1283cc64d762215c29007047f
Pedro Pereira <pfpereira2004@gmail.com>
2024-05-17 23:28:48 +0100

    chore: null control

--- Lusium.sql
+++ Lusium.sql
@@ -4,33 +4,33 @@ USE Lusium;
 
 -- Criação da tabela Função
 CREATE TABLE Função (
-    Função_ID INT PRIMARY KEY,
-    Designação ENUM('Operacional', 'Detetive', 'Representante')
+    Função_ID INT PRIMARY KEY NOT NULL,
+    Designação ENUM('Operacional', 'Detetive', 'Representante') NOT NULL
 );
 
 -- Criação da tabela Funcionário
 CREATE TABLE Funcionário (
-    Funcionário_ID INT PRIMARY KEY,
-    Nome VARCHAR(75),
-    Data_de_nascimento DATE,
-    Salário INT,
-    NIF VARCHAR(10),
-    Fotografia VARCHAR(150),
-    Função_ID INT,
+    Funcionário_ID INT PRIMARY KEY NOT NULL,
+    Nome VARCHAR(75) NOT NULL,
+    Data_de_nascimento DATE NOT NULL,
+    Salário INT NOT NULL,
+    NIF VARCHAR(10) NOT NULL,
+    Fotografia VARCHAR(150) NULL,
+    Função_ID INT NOT NULL,
     FOREIGN KEY (Função_ID) REFERENCES Função(Função_ID)
 );
 
 -- Criação da tabela Número de telemóvel
 CREATE TABLE Número_de_telemóvel (
-    Número_de_telemóvel_ID INT PRIMARY KEY,
-    Funcionário_ID INT,
+    Número_de_telemóvel_ID INT PRIMARY KEY NOT NULL,
+    Funcionário_ID INT NOT NULL,
     FOREIGN KEY (Funcionário_ID) REFERENCES Funcionário(Funcionário_ID)
 );
 
 -- Criação da tabela Gere
 CREATE TABLE Gere (
-    Funcionário_Gestor_ID INT,
-    Funcionário_ID INT,
+    Funcionário_Gestor_ID INT NOT NULL,
+    Funcionário_ID INT NOT NULL,
     PRIMARY KEY (Funcionário_Gestor_ID, Funcionário_ID),
     FOREIGN KEY (Funcionário_Gestor_ID) REFERENCES Funcionário(Funcionário_ID),
     FOREIGN KEY (Funcionário_ID) REFERENCES Funcionário(Funcionário_ID)
@@ -38,15 +38,15 @@ CREATE TABLE Gere (
 
 -- Criação da tabela Terreno
 CREATE TABLE Terreno (
-    Terreno_ID INT PRIMARY KEY,
-    Minério_previsto INT,
-    Minério_coletado INT
+    Terreno_ID INT PRIMARY KEY NOT NULL,
+    Minério_previsto INT NOT NULL,
+    Minério_coletado INT NOT NULL
 );
 
 -- Criação da tabela Trabalha
 CREATE TABLE Trabalha (
-    Funcionário_ID INT,
-    Terreno_ID INT,
+    Funcionário_ID INT NOT NULL,
+    Terreno_ID INT NOT NULL,
     PRIMARY KEY (Funcionário_ID, Terreno_ID),
     FOREIGN KEY (Funcionário_ID) REFERENCES Funcionário(Funcionário_ID),
     FOREIGN KEY (Terreno_ID) REFERENCES Terreno(Terreno_ID)
@@ -54,22 +54,22 @@ CREATE TABLE Trabalha (
 
 -- Criação da tabela Caso
 CREATE TABLE Caso (
-    Caso_ID INT PRIMARY KEY,
-    Data_de_abertura DATE,
-    Estado ENUM('Aberto', 'Fechado'),
-    Estimativa_de_roubo INT,
-    Data_de_encerramento DATE,
-    Terreno_ID INT,
+    Caso_ID INT PRIMARY KEY NOT NULL,
+    Data_de_abertura DATE NOT NULL,
+    Estado ENUM('Aberto', 'Fechado') NOT NULL,
+    Estimativa_de_roubo INT NOT NULL,
+    Data_de_encerramento DATE NULL,
+    Terreno_ID INT NOT NULL,
     FOREIGN KEY (Terreno_ID) REFERENCES Terreno(Terreno_ID)
 );
 
 -- Criação da tabela Suspeito
 CREATE TABLE Suspeito (
-    Funcionário_ID INT,
-    Caso_ID INT,
-    Estado ENUM('Inocente', 'Em investigação', 'Culpado'),
-    Envolvimento INT,
-    Notas TEXT,
+    Funcionário_ID INT NOT NULL,
+    Caso_ID INT NOT NULL,
+    Estado ENUM('Inocente', 'Em investigação', 'Culpado') NOT NULL,
+    Envolvimento INT NOT NULL,
+    Notas TEXT NULL,
     PRIMARY KEY (Funcionário_ID, Caso_ID),
     FOREIGN KEY (Funcionário_ID) REFERENCES Funcionário(Funcionário_ID),
     FOREIGN KEY (Caso_ID) REFERENCES Caso(Caso_ID)
