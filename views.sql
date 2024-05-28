c5854abfdd53e451b50ab6050b903ebb7a0c84eb
Pedro Pereira <109802203+pedrofp4444@users.noreply.github.com>
2024-05-28 20:50:54 +0100

    chore: fix typo in name of view

--- views.sql
+++ views.sql
@@ -1,5 +1,5 @@
 -- View das informações dos funcionários e dos casos associados.
-CREATE VIEW Funcionarios_Em_Casos AS
+CREATE VIEW FuncionariosEmCasos AS
 SELECT
     Funcionário.Nome,
     Número_de_telemóvel.Número_de_telemóvel_ID,
@@ -19,7 +19,7 @@ LEFT JOIN
     Número_de_telemóvel ON Funcionário.Funcionário_ID = Número_de_telemóvel.Funcionário_ID;
 
 -- View das informações dos funcionários e dos terrenos associados.
-CREATE VIEW Funcionarios_Em_Terrenos AS
+CREATE VIEW FuncionariosEmTerrenos AS
 SELECT
 	Funcionário.Nome,
     Função.Designação,
@@ -39,5 +39,3 @@ INNER JOIN
 	Função ON Funcionário.Função_ID = Função.Função_ID
 LEFT JOIN
 	Número_de_telemóvel ON Funcionário.Funcionário_ID = Número_de_telemóvel.Funcionário_ID;
-    
-    
